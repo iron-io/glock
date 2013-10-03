5468366cd64a02f5830a21ed3217227689382674
Evan Shaw <evan@iron.io>
2013-10-03 16:58:49 -0500

    Initial code

---
+++ glock.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type timeoutLock struct {
+	mutex sync.Mutex
+	id    int64 // unique ID of the current lock. Only allow an unlock if the correct id is passed
+}
+
+var locksLock sync.RWMutex
+var locks = map[string]*timeoutLock{}
+
+func main() {
+	var port int
+	flag.IntVar(&port, "p", 9845, "port")
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatalln("error listening", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("error accepting", err)
+			return
+		}
+		go handleConn(conn)
+	}
+}
+
+var (
+	lockedResponse   = []byte("LOCKED\r\n")
+	unlockedResponse = []byte("UNLOCKED\r\n")
+
+	errBadFormat      = []byte("ERROR bad command format\r\n")
+	errUnknownCommand = []byte("ERROR unknown command\r\n")
+	errLockNotFound   = []byte("ERROR lock not found\r\n")
+)
+
+func handleConn(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		split := strings.Fields(scanner.Text())
+		if len(split) < 3 {
+			conn.Write(errBadFormat)
+			continue
+		}
+		cmd := split[0]
+		key := split[1]
+		switch cmd {
+		// LOCK <key> <timeout>
+		case "LOCK":
+			timeout, err := strconv.Atoi(split[2])
+			if err != nil {
+				conn.Write(errBadFormat)
+				continue
+			}
+			locksLock.RLock()
+			lock, ok := locks[key]
+			locksLock.RUnlock()
+			if !ok {
+				// lock doesn't exist; create it
+				locksLock.Lock()
+				lock, ok = locks[key]
+				if !ok {
+					lock = &timeoutLock{}
+					locks[key] = lock
+				}
+				locksLock.Unlock()
+			}
+
+			lock.mutex.Lock()
+			id := atomic.AddInt64(&lock.id, 1)
+			time.AfterFunc(time.Duration(timeout)*time.Millisecond, func() {
+				if atomic.CompareAndSwapInt64(&lock.id, id, id+1) {
+					lock.mutex.Unlock()
+				}
+			})
+			fmt.Fprintf(conn, "LOCKED %v\r\n", id)
+
+		// UNLOCK <key> <id>
+		case "UNLOCK":
+			id, err := strconv.ParseInt(split[2], 10, 64)
+			if err != nil {
+				conn.Write(errBadFormat)
+				continue
+			}
+			locksLock.RLock()
+			lock, ok := locks[key]
+			locksLock.RUnlock()
+			if !ok {
+				conn.Write(errLockNotFound)
+				continue
+			}
+			if atomic.CompareAndSwapInt64(&lock.id, id, id+1) {
+				lock.mutex.Unlock()
+				conn.Write(unlockedResponse)
+			} else {
+				conn.Write(lockedResponse)
+			}
+
+		default:
+			conn.Write(errUnknownCommand)
+			continue
+		}
+	}
+}
