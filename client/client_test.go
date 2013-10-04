de8011552e5f7d7906d9e3a161c97f27532e0d6d
sidney zhang <sidazhang89@gmail.com>
2013-10-03 17:41:35 -0700

    Some formatting

--- client/client_test.go
+++ client/client_test.go
@@ -8,21 +8,30 @@ import (
 
 func TestLockUnlock(t *testing.T) {
 	client := NewClient("localhost:45625")
+
 	fmt.Println("1 getting lock")
 	id1 := client.LockEx("x", 10000)
 	fmt.Println("1 got lock")
+
 	go func() {
+
 		fmt.Println("2 getting lock")
 		id2 := client.LockEx("x", 10000)
 		fmt.Println("2 got lock")
+
 		fmt.Println("2 releasing lock")
 		client.UnLock("x", id2)
 		fmt.Println("2 released lock")
+
 	}()
+
 	fmt.Println("sleeping")
 	time.Sleep(5 * time.Second)
+	fmt.Println("finished sleeping")
+
 	fmt.Println("1 releasing lock")
 	client.UnLock("x", id1)
 	fmt.Println("1 released lock")
+
 	time.Sleep(5 * time.Second)
 }
