aa250b1cbee900e4e6565dce894db6d2078db6e9
sidney zhang <sidazhang89@gmail.com>
2013-10-14 16:47:04 -0700

    Check endpoint status first before adding into the hash table

--- client/hashing.go
+++ client/hashing.go
@@ -1,14 +1,19 @@
 package glock
 
 import (
+	"net"
+
 	"github.com/stathat/consistent"
 )
 
 func initServersPool(endpoints []string) *consistent.Consistent {
 	cons := consistent.New()
 	for _, endpoint := range endpoints {
-		// TODO: First check if endpoint is live
-		cons.Add(endpoint)
+		conn, err := net.Dial("tcp", endpoint)
+		if err == nil {
+			cons.Add(endpoint)
+			conn.Close()
+		}
 	}
 	return cons
 }
