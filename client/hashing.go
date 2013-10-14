4f805d591e54f59cf4042a03b82940dc638dbacb
sidney zhang <sidazhang89@gmail.com>
2013-10-14 16:32:34 -0700

    Glock Client to handle multiple glockServers
    Glock Client will handle multipel glock servers, if one server drops
    due to a connection error, the client will drop the server from its
    consistent hashing list. And then recursively attempt to lock again.
    
    All consistent hashing calls are already protected by a lock inside the
    library so we don't need to do any mutex locking while using them.

--- client/hashing.go
+++ client/hashing.go
@@ -4,10 +4,10 @@ import (
 	"github.com/stathat/consistent"
 )
 
-func initServesPool(endpoints []string) *consistent.Consistent {
+func initServersPool(endpoints []string) *consistent.Consistent {
 	cons := consistent.New()
 	for _, endpoint := range endpoints {
-		// First check if endpoint is live
+		// TODO: First check if endpoint is live
 		cons.Add(endpoint)
 	}
 	return cons
